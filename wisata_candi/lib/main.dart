8dd0dad1d01d7bf5fceb65a9ebee770f84e4fcca
adit <aditdiscord17@gmail.com>
2024-11-11 11:28:42 +0700

    Part 1- main.dart

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -10,11 +10,25 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      title: 'Wisata Candi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true
+      ),
+      home: DetailScreen()(
+
       ),
     );
   }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Placeholder();
+  }
+
 }
